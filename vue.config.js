504dba9a06f41eb27cbed8bc725eee347445c503
zhuhuanhuan <zhuhuanhuan@daojia-inc.com>
2020-07-09 19:01:20 +0800

    feat：vue配置

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,18 @@ module.exports = {
         },
       }
     }
+  },
+  devServer: {
+    allowedHosts: [
+      '.djtest.cn'
+    ],
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        pathRewrite: {
+          '^/api': '',
+        }
+      }
+    }
   }
 }
